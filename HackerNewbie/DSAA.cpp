64f4fa3d73f22e509a1aa25b9da260cf57c32d84
Ebenezer Scrooge <1641367382@qq.com>
2020-08-23 12:21:27 +0800

    Update DSAA.cpp

--- HackerNewbie/DSAA.cpp
+++ HackerNewbie/DSAA.cpp
@@ -6,12 +6,25 @@ extern void Iamastudent();
 extern void star_tower(int max, int now);
 extern void sphere_surface();
 
+int a = 0;
+
+int func1() {
+	a = 2;
+	return 2;
+}
+
+int func2() {
+	a = 3;
+	return 3;
+}
+
+int func3() {
+	a = 4;
+	return 4;
+}
 
 int main() {
 	//sphere_surface();
-	char i[3] = {"ab"};
-	char* ip=i;
-	cout << *ip << "\n";
-	ip += 1;
-	cout << *ip << "\n";
+	int a = 1;
+	int c = ++a * -a;
 }
