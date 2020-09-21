633ab20e2b7b3d8d5c9bd24b79ee56b48adb75b7
Ebenezer Scrooge <1641367382@qq.com>
2020-09-21 09:32:52 +0800

    Add OS_Linux

---
+++ OS_Linux/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdio>
+#include <stdlib.h>
+
+
+int main()
+{
+    char* name;
+    char* path;
+
+    name = getenv("LOGNAME");
+    path = getenv("PWD");
+
+    printf("logname = %s\n", name);
+    printf("pwd = %s\n", path);
+
+    return 0;
+}
