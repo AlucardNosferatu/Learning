6d7550174ea466203e6e806b79fc5c376d5c0fd1
Ebenezer Scrooge <1641367382@qq.com>
2020-09-21 09:44:02 +0800

    Update main.cpp

--- OS_Linux/main.cpp
+++ OS_Linux/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <cstdio>
 #include <stdlib.h>
-
+#include <sys/utsname.h>
 
 int main()
 {
@@ -12,6 +12,21 @@ int main()
 
     printf("logname = %s\n", name);
     printf("pwd = %s\n", path);
+
+
+    struct utsname buf;
+
+    if (uname(&buf))
+    {
+        perror("uname");
+        exit(1);
+    }
+
+    printf("sysname:%s\n", buf.sysname);
+    printf("nodename:%s\n", buf.nodename);
+    printf("release:%s\n", buf.release);
+    printf("version:%s\n", buf.version);
+    printf("machine:%s\n", buf.machine);
 
     return 0;
 }
