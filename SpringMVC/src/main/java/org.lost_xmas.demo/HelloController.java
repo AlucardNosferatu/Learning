6030caac93f1707cad0e4826aab12ebfb7ea675a
Ebenezer Scrooge <1641367382@qq.com>
2021-02-24 20:24:21 +0800

    Fuck Tomcat

--- SpringMVC/src/main/java/org.lost_xmas.demo/HelloController.java
+++ SpringMVC/src/main/java/org.lost_xmas.demo/HelloController.java
@@ -1,9 +1,8 @@
-package org.lost_xmas.demo;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
+
 @Controller
 @RequestMapping("/hello")
 public class HelloController{
