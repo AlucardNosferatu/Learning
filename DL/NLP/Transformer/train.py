f2d9ece70d95cc5a0a712d8784860e5144131705
Ebenezer Scrooge <1641367382@qq.com>
2023-03-26 18:44:08 +0800

    Update train.py

--- DL/NLP/Transformer/train.py
+++ DL/NLP/Transformer/train.py
@@ -29,31 +29,34 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 
-model = transformer(
-    vocab_size=TARGET_VOCAB_SIZE + 2,
-    num_layers=NUM_LAYERS,
-    units=UNITS,
-    d_model=D_MODEL,
-    num_heads=NUM_HEADS,
-    dropout=DROPOUT)
-print('模型初始化完成')
-learning_rate = CustomSchedule(D_MODEL)
-print('学习率规划完成')
-optimizer = tf.keras.optimizers.Adam(
-    learning_rate,
-    beta_1=0.9,
-    beta_2=0.98,
-    epsilon=1e-9
-)
-print('优化器初始化完成')
-model.compile(
-    optimizer=optimizer,
-    loss=loss_function,
-    metrics=[accuracy, perplexity],
-    run_eagerly=True
-)
-print('模型编译完成')
-increment = True
+def prepare_model():
+    model = transformer(
+        vocab_size=TARGET_VOCAB_SIZE + 2,
+        num_layers=NUM_LAYERS,
+        units=UNITS,
+        d_model=D_MODEL,
+        num_heads=NUM_HEADS,
+        dropout=DROPOUT)
+    print('模型初始化完成')
+    learning_rate = CustomSchedule(D_MODEL)
+    print('学习率规划完成')
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate,
+        beta_1=0.9,
+        beta_2=0.98,
+        epsilon=1e-9
+    )
+    print('优化器初始化完成')
+    model.compile(
+        optimizer=optimizer,
+        loss=loss_function,
+        metrics=[accuracy, perplexity],
+        run_eagerly=True
+    )
+    print('模型编译完成')
+    return model
+
+
 if __name__ == '__main__':
     questions, answers = load_conversations_from_json('Data/dataset.json')
     questions2, answers2 = load_conversations_from_csv('Data/20200325_counsel_chat.csv')
@@ -63,12 +66,14 @@ if __name__ == '__main__':
     dataset = dataset.batch(BATCH_SIZE)
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     print('数据集分批+配置预取完成')
+    mdl = prepare_model()
+    increment = True
     if increment:
-        model.load_weights('Save/bot_4')
+        mdl.load_weights('Save/bot_4')
     for i in range(0, EPOCHS):
         print('当前周期：', i + 1)
         with tf.device('/gpu:0'):
-            model.fit(dataset, epochs=1)
+            mdl.fit(dataset, epochs=1)
         if (i + 1) % SAVE_PERIOD == 0:
-            model.save_weights('Save/bot_4')
+            mdl.save_weights('Save/bot_4')
             print('训练进度已保存')
