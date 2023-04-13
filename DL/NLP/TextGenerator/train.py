9acf0bebc50303564119a70d8879223f8af505fe
Ebenezer Scrooge <1641367382@qq.com>
2023-04-13 10:57:45 +0800

    Update

--- DL/NLP/TextGenerator/train.py
+++ DL/NLP/TextGenerator/train.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 from config import noise_dim, weight_path
 from data import spawn_data_seq
-from model.CGAN import ConditionalGAN
+from model.CGAN import ConditionalGAN, train_dis
 from model.discriminator import spawn_d
 from model.generator import spawn_g
 
@@ -14,9 +14,13 @@ cond_gan.compile(
     g_optimizer=tf.keras.optimizers.Adam(learning_rate=0.0003),
     loss_fn=tf.keras.losses.BinaryCrossentropy(from_logits=True),
 )
+if train_dis:
+    save_by_loss = 'd_loss'
+else:
+    save_by_loss = 'g_loss'
 ckpt = tf.keras.callbacks.ModelCheckpoint(
     weight_path,
-    monitor='d_loss',
+    monitor=save_by_loss,
     save_best_only=True,
     save_weights_only=True,
     mode='min',
