292edaf28eba1c30e4b19c8dd12966ce6e4d2a38
Ebenezer Scrooge <1641367382@qq.com>
2023-04-13 11:31:25 +0800

    Update generator.py

--- DL/NLP/TextGenerator/model/generator.py
+++ DL/NLP/TextGenerator/model/generator.py
@@ -22,7 +22,7 @@ def spawn_g():
             tf.keras.layers.LeakyReLU(alpha=0.2),
             tf.keras.layers.Conv2DTranspose(64, (4, 4), strides=(stride2, stride2), padding="same"),
             tf.keras.layers.LeakyReLU(alpha=0.2),
-            tf.keras.layers.Conv2D(1, (k_size, k_size), padding="same", activation="sigmoid")
+            tf.keras.layers.Conv2D(1, (8, 8), padding="same", activation="sigmoid")
         ],
         name="generator",
     )
